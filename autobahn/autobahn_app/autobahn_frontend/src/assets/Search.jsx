81e8d94729e48cd26edbbffad4a3bdb23564a7d4
Lawrence Lin <lawrencelin1154@gmail.com>
2024-04-20 07:42:17 -0400

    Made search button

--- autobahn/autobahn_app/autobahn_frontend/src/assets/Search.jsx
+++ autobahn/autobahn_app/autobahn_frontend/src/assets/Search.jsx
@@ -1,37 +1,57 @@
-import React from 'react';
-import TextField from '@mui/material/TextField';
+import React from "react";
+import TextField from "@mui/material/TextField";
+import { FormControl } from "@mui/material";
+import { Button } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 
 function Search() {
   return (
-    <TextField
-      style={{
-        borderRadius: '5px',
-        textTransform: 'none',
-        background: '#757575',
-        width: '350px',
-      }}
-      sx={{
-        color: '#f5f5f5',
-        border: 'solid',
-        "& label": {
-          color: '#f5f5f5',
-          "&.Mui-focused": {
-            color: '#f5f5f5'
-          }
-        },
-        "& .MuiInputBase-input": {
-          color: '#f5f5f5',
-          height: '15px', // Adjusted height
-        },
-      }}
-      label="Search"
-      variant="filled"
-      id="filled-basic"
-      size='small'
+    <FormControl
+      sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}
     >
-    </TextField>
+      <TextField
+        style={{
+          borderRadius: "5px",
+          textTransform: "none",
+          background: "#757575",
+          width: "350px",
+        }}
+        sx={{
+          color: "#f5f5f5",
+          border: "solid",
+          "& label": {
+            color: "#f5f5f5",
+            "&.Mui-focused": {
+              color: "#f5f5f5",
+            },
+          },
+          "& .MuiInputBase-input": {
+            color: "#f5f5f5",
+            height: "15px",
+          },
+        }}
+        label="Search"
+        variant="filled"
+        id="filled-basic"
+        size="small"
+      ></TextField>
+      <Button
+        variant="outlined"
+        style={{
+          border: "2px solid",
+          textTransform: "none",
+          background: "#757575",
+          maxHeight: "46px",
+          minHeight: "46px",
+          minWidth: "100px",
+          maxWidth: "100px",
+        }}
+        sx={{ color: "#f5f5f5" }}
+      >
+        <SearchIcon />
+      </Button>
+    </FormControl>
   );
 }
 
 export default Search;
-
