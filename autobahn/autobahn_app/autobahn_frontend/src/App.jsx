98a069f8808bd45d7fd9c039948ef9fa54861d72
Lawrence Lin <lawrencelin1154@gmail.com>
2024-04-20 15:54:48 -0400

    Working header

--- autobahn/autobahn_app/autobahn_frontend/src/App.jsx
+++ autobahn/autobahn_app/autobahn_frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from '@emotion/react';
 import './App.css'
 import AboutPage from './assets/AboutPage';
 import Home from './assets/Home';
+import SellForm from './assets/SellForm';
 function App() {
 
 const theme = createTheme({
@@ -25,8 +26,10 @@ const theme = createTheme({
     <ThemeProvider theme={theme}>
       <BrowserRouter>
       <Routes>
+        <Route index element={<Home />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/AboutPage" element={<AboutPage />} />
+        <Route path="/SellForm" element={<SellForm />} />
       </Routes>
       </BrowserRouter>
     </ThemeProvider>
