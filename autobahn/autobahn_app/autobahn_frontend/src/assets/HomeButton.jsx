7f90bbcce965b3e742efdb58e066cd0bdd7662e9
javierarana115 <javierarana115@gmail.com>
2024-04-20 03:32:46 -0400

    Header update

--- autobahn/autobahn_app/autobahn_frontend/src/assets/HomeButton.jsx
+++ autobahn/autobahn_app/autobahn_frontend/src/assets/HomeButton.jsx
@@ -3,7 +3,7 @@ import { Button } from '@mui/material';
 
 function HomeButton(){
     return(
-        <Button variant="outlined" style={{border: '2px solid', textTransform: 'none', backgroundColor:'#757575'}} sx={{color:'#f5f5f5'}}>
+        <Button variant="outlined" style={{border: '2px solid', textTransform: 'none', backgroundColor:'#757575',maxHeight: '40px', minHeight: '40px', minWidth: '100px', maxWidth: '100px'}} sx={{color:'#f5f5f5'}}>
             Home
         </Button>
     );
--- autobahn/autobahn_app/autobahn_frontend/src/assets/SellACar.jsx
+++ autobahn/autobahn_app/autobahn_frontend/src/assets/SellACar.jsx
@@ -3,7 +3,7 @@ import { Button } from '@mui/material';
 
 function SellACar(){
     return(
-        <Button variant="outlined" style={{border: '2px solid', textTransform: 'none', background:'#757575'}} sx={{color:'#f5f5f5'}}>
+        <Button variant="outlined" style={{border: '2px solid', textTransform: 'none', background:'#757575', maxHeight: '40px', minHeight: '40px', minWidth: '100px', maxWidth: '100px'}} sx={{color:'#f5f5f5'}}>
             Sell A Car
         </Button>
     );
